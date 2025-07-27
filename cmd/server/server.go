20aa3a5b532b86b8a73ba2ba9b49d9f8301b763d
Will Bartholomay <112407709+wbartholomay@users.noreply.github.com>
2025-07-27 19:40:19 -0400

    Develop (#10)
    * updated readfme
    
    * readme update
    
    * wip
    
    * getting to endgame
    
    * play again working
    
    * switched quitting to conceding while in game
    
    * committing before claude wreaks havoc on checkers display
    
    * claude about to destroy everything it has worked so hard to build
    
    * checkers display working
    
    * reversed move direction for black
    
    * checkers functinoal
    
    * adding binary for install
    
    * Update README.md
    
    * wip
    
    * fixing folder structure

---
+++ cmd/server/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	// _ "net/http/pprof"
+)
+
+func main() {
+	log.Println("Starting server...")
+	hub := NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/", hub.ServeWs)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
